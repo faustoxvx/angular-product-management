9dc6a3ff06a2a75edb9501b2e9406686737c5032
Fausto Nunes <faustornunes@gmail.com>
2023-10-10 12:08:18 +0100

    Add custom convertToSpaces pipe

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('in OnInit');
+  }
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
